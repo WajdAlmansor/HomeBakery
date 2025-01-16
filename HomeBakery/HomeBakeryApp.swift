fd3d1bc313b217d7f85eb381b9eb88911a85b6cd
WajdButterfly <wajd2828@gmail.com>
2025-01-16 15:03:52 +0300

    Initial Commit

---
+++ HomeBakery/HomeBakeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeBakeryApp.swift
+//  HomeBakery
+//
+//  Created by Wajd on 16/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct HomeBakeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
